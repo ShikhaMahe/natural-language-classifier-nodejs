37318a800f5d6a9141db49537c9e16dfe72a2387
Shikha Maheshwari <shikha.mah@in.ibm.com>
2016-12-06 07:16:01 +0000

    Created new classifier with new train data and modify the same into app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 app.post("/api/classify", (req, res, next) =>
   classifier.classify({
     text: req.body.text,
-    classifier_id: process.env.CLASSIFIER_ID || "8d6cd8x123-nlc-5770",
+    classifier_id: process.env.CLASSIFIER_ID || "8d6cd8x123-nlc-5851",
   }, (err, data) => {
     if (err) {
       return next(err);
